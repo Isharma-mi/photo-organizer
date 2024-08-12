8201812d461d4043606750e4b691c9684ab2de71
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-08-12 17:16:25 -0400

    Maven conversion
    - Converted project to maven to more easily add dependencies like for logging and maybe GUI in long run
    - Added in src/main/java and src/main/resources folders
    - Got logger to work
    - TODO: Setup logger with things like Rolling
    - TODO: Get functionality added

---
+++ src/main/java/com/example/PhotoOrganizer/logic/PhotoOrganizerApplication.java
@@ -0,0 +1,11 @@
+package com.example.PhotoOrganizer.logic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PhotoOrganizerApplication {
+	public static void main(String[] args) {
+		System.out.println("In PhotoOrganzierApplication");
+		//Logger logger = LoggerFactory.getLogger(PhotoOrganizerApplication.class);
+	}
+}
