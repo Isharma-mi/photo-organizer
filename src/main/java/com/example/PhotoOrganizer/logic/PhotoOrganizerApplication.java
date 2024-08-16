615768e6f7bca9bb12498f4e7aea613cd5892985
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-08-15 21:49:13 -0400

    Cleaned up folder verification code
    - Moved folder verification code to a sep method to repeat process to also verify destination folder exists
    - TODO: Loop thru each file and rename if needed
    - TODO: Create folders in another directory with subfolders for years and months

--- src/main/java/com/example/PhotoOrganizer/logic/PhotoOrganizerApplication.java
+++ src/main/java/com/example/PhotoOrganizer/logic/PhotoOrganizerApplication.java
@@ -1,7 +1,7 @@
 package com.example.PhotoOrganizer.logic;
 
-import java.io.IOException
-import java.nio.file.DirectoryStream;;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,41 +10,42 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PhotoOrganizerApplication {
+	static Logger logger = LoggerFactory.getLogger(PhotoOrganizerApplication.class);
+	
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(PhotoOrganizerApplication.class);
+		
 		logger.info("Application started...");
 		
 		// Gets pathway from user
 		System.out.println("What is the pathway of the root folder of photos?");
 		Scanner scanner = new Scanner(System.in);
 		final String pathway = scanner.nextLine();
-		Path rootFolder = Paths.get(pathway);
+		Path rootDirectory = Paths.get(pathway);
 		
-		// Verify that a folder was given
-		if (!Files.exists(rootFolder)) {
-			logger.error("Root folder unable to be found!");
+		// Exit out of the program if unable to find the root folder
+		if (!verifyDirectoryExists(rootDirectory)) {
+			System.out.println("ERROR: Unable to find the root folder. Program terminating.");
 			return;
-		} else {
-			logger.info("Root folder was found.");
-			logger.info("Address used: " + rootFolder.toString());
+		}
+
+		// Gets destination where organized files will be
+		System.out.println("Where would you like to deposit the folders? "
+				+ "A folder will be created there which will contain all the subfolders.");
+		final String destinationPathway = scanner.nextLine();
+		Path destinationDirectory = Paths.get(destinationPathway);
+		
+		// Exit out of program if unable to find the destination folder
+		if (!verifyDirectoryExists(destinationDirectory)) {
+			System.out.println("ERROR: Unable to find the destination folder. Program terminating.");
 		}
 		
+		
 		// Loop thru the files in the directory
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(rootFolder)) {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(rootDirectory)) {
 			
 		} catch (IOException e) {
 			logger.error("Unable to process thru directory");
 		}
-		
-		/*
-		try {
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		*/
-		
-		
 		
 		// Try to access the pathway
 		// Get pathway to create organized folders
@@ -53,7 +54,24 @@ public class PhotoOrganizerApplication {
 		// Put file in the specific folder
 	}
 	
-	private void analyzeFile() {
+	/**
+	 * Verifies that the given pathway exists as a directory in the computer .
+	 * @param directory Path to check if it is a directory that exists.
+	 * @return returns boolean letting caller know if folder exists
+	 */
+	private static boolean verifyDirectoryExists(Path directory) {
+		if (!Files.exists(directory)) {
+			logger.error("Unable to find the directory!");
+			logger.error("Address used: " + directory.toString());
+			return false;
+		} 
+
+		logger.info("Directory was found.");
+		logger.info("Address used: " + directory.toString());
+		return true;
+	}
+	
+	private static void analyzeFile() {
 		
 	}
 }
